85c169ab325f80ff75e6a23dde4c827ea2ac5ac3
AhhhhYe <hiddendreams3112@gmail.com>
2024-01-22 10:57:30 +0800

    Updated CalculatorScript
    Updated Script to work with buttons and to not produce a value if both toggles are true

--- 27_GuanKai_Lab08/Assets/CalculatorScript.cs
+++ 27_GuanKai_Lab08/Assets/CalculatorScript.cs
@@ -8,6 +8,7 @@ public class CalculatorScript : MonoBehaviour
     public Toggle USD;
     public Toggle Yen;
     public InputField txtAmount;
+    public Text Debugtxt;
     float value;
     float SGDamount;
     // Start is called before the first frame update
@@ -19,21 +20,38 @@ public class CalculatorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        conversion();
+
     }
     public void conversion()
     {
         SGDamount = float.Parse(txtAmount.text);
+        
 
         if (USD.GetComponent<Toggle>().isOn == true)
         {
+            Debugtxt.text = "";
             value = SGDamount * 0.74F;
             txtConverted.text = value.ToString() + " USD";
         }
         if (Yen.GetComponent<Toggle>().isOn == true)
         {
+            Debugtxt.text = "";
             value = SGDamount * 82.78f;
             txtConverted.text = value.ToString() + " JPN";
         }
+        if (USD.GetComponent<Toggle>().isOn == true&& Yen.GetComponent<Toggle>().isOn == true)
+        {
+            Debugtxt.text = "Please Select Only One";
+            txtConverted.text = "";
+        }
+        else if (USD.GetComponent<Toggle>().isOn == false && Yen.GetComponent<Toggle>().isOn == false)
+        {
+            Debugtxt.text = "Please Select One";
+        }
+    }
+    public void clear()
+    {
+        txtAmount.text = "";
+        txtConverted.text = "";
     }
 }
